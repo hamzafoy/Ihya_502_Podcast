dc763ace3346f9b3ca68cc512e55853b560f520c
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2020-12-24 04:08:55 -0500

    adjust the absolute URLs for the play/stop button.

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 let audioPlayer = document.getElementById("audio_player");
 let podcast = document.getElementById("podcast");
-let audio = audioPlayer.src;
-let test1 = "http://localhost/img/play_button.svg";
 
 audioPlayer.addEventListener("click", () => {
-  let gauge_if_playing = (audioPlayer.src == test1) ? true : false;
+  let gauge_if_playing = (audioPlayer.src == "http://ihya502.surge.sh/img/play_button.svg") ? true : false;
   console.log(gauge_if_playing);
   if(gauge_if_playing) {
-  audioPlayer.src = "http://localhost/img/stop_button.svg";
-  podcast.pause();
-  } else {
-  audioPlayer.src = "http://localhost/img/play_button.svg";
+  audioPlayer.src = "http://ihya502.surge.sh/img/stop_button.svg";
   podcast.play();
+  } else {
+  audioPlayer.src = "http://ihya502.surge.sh/img/play_button.svg";
+  podcast.pause();
   }
 });

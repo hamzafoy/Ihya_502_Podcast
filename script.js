24c5fa1669993ab52f51d85c88cc86aab2226bdb
Ross Hamza Foy <hamzaoflouisville@gmail.com>
2020-12-24 03:51:16 -0500

    tweaking the play/stop button.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let audioPlayer = document.getElementById("audio_player");
 let podcast = document.getElementById("podcast");
 let audio = audioPlayer.src;
 let test1 = "http://localhost/img/play_button.svg";
-console.log(audio);
 
 audioPlayer.addEventListener("click", () => {
   let gauge_if_playing = (audioPlayer.src == test1) ? true : false;
+  console.log(gauge_if_playing);
   if(gauge_if_playing) {
   audioPlayer.src = "http://localhost/img/stop_button.svg";
   podcast.pause();
